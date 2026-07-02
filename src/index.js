d9fe22aacde3afaa47503c6a9cd857131cc7e660
Siddharth-732 <siddharthpn2003@gmail.com>
2026-07-02 21:30:18 +0530

    feat: initialize email microservice

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+import http from "http";
 import { connectDB } from "./config/db.js";
 import "./config/redisClient.js";
 import "./workers/emailWorker.js";
 
-// Connect to MongoDB
 connectDB();
 
 console.log(" Email Microservice is running and listening for jobs...");
+
+// Dummy HTTP server to pass Render's Web Service health check
+const PORT = process.env.PORT || 4000;
+http
+  .createServer((req, res) => {
+    res.writeHead(200);
+    res.end("Bind Email Service is running in the background.");
+  })
+  .listen(PORT, () => {
+    console.log(` Dummy Health Check server listening on port ${PORT}`);
+  });
