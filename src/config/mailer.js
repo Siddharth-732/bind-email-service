89a2af181be9128b3cac4458418150a803abc99b
Siddharth-732 <siddharthpn2003@gmail.com>
2026-07-03 22:05:51 +0530

    fix: ip mismatch

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -12,9 +12,8 @@ const transporter = nodemailer.createTransport({
     pass: process.env.SMTP_PASS,
   },
   tls: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
   },
-  // Force IPv4 to prevent ENETUNREACH errors on misconfigured IPv6 networks
   family: 4,
 });
 
